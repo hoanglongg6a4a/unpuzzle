b29114830edfde063c0f9d949748b2a2d3041baa
Trần Hoàng Long <tranhoanglong.stt.60@gmail.com>
2023-06-01 03:44:20 +0700

    Up

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Data.Common;
 using Unity.VisualScripting;
 using UnityEngine;
+public enum NodeType
+{
+    None , Left , Right , Up , Down
+}
 public class GameController : MonoBehaviour
 {
     [Header("MVC")]
@@ -12,42 +16,13 @@ public class GameController : MonoBehaviour
     [Header("Preference")]
     [SerializeField] SpawnPuzzle spawnPuzzle;
     private Camera mainCamera;
-    private Puzzle puzzle;
-    private GameObject puzzleObject;
     private void Awake()
     {
         mainCamera = Camera.main;
-        spawnPuzzle.init(3,3);
+        //spawnPuzzle.init(5,5,1);
     }
     private void Update()
     {
-        Puzzle puzzleClick = CheckClick();
-        if (puzzleClick != null)
-        {
-            puzzleObject = puzzleClick.gameObject; 
-        }
-        if (puzzleObject != null)
-        {
-            puzzle = puzzleObject.GetComponent<Puzzle>();
-        }
+ 
     }
-    private Puzzle CheckClick()
-    {
-        if (Input.GetMouseButtonDown(0)) // Kiểm tra nút chuột trái được nhấn xuống
-        {
-            RaycastHit2D hit = Physics2D.Raycast(mainCamera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); // Thực hiện Raycast từ vị trí chuột
-            if (hit.collider != null) // Kiểm tra nếu Raycast chạm vào collider
-            {
-                Puzzle puzzle = hit.collider.GetComponent<Puzzle>(); // Lấy tham chiếu đến component Puzzle của game object được chạm
-
-                if (puzzle != null) // Kiểm tra nếu component Puzzle tồn tại
-                {
-                    puzzle.init(model.Speed,spawnPuzzle.IsPositionEmpty,spawnPuzzle.GetCellPosition,spawnPuzzle.SetPuzzlePosition,spawnPuzzle.DeletePos); // Gọi phương thức init để bắt đầu di chuyển
-                    return puzzle; // Trả về đối tượng Puzzle được click vào
-                }
-            }
-        }
-        return null; // Trả về null nếu không có đối tượng Puzzle được click vào
-    }
-
 }
--- Assets/Scripts/Puzzle/SpawnPuzzle.cs
+++ Assets/Scripts/Puzzle/SpawnPuzzle.cs
@@ -2,120 +2,151 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+[System.Serializable]
+public class Node
+{
+    public Vector2Int position;
+    public NodeType NodeType;
+
+    public Node(Vector2Int position, NodeType nodeType)
+    {
+        this.position = position;
+        NodeType = nodeType;
+    }
+}
 public class SpawnPuzzle : MonoBehaviour
 {
+    [SerializeField]   
+    private int rows;
     [SerializeField]
-    private List<Puzzle> spawnPuzzleList;
-    private Puzzle[,] grid;
-    private int rows; 
-    private int columns;
-    private bool[,] blocked;
-    private List<Puzzle> puzzlePrefabs = new List<Puzzle>();
+    private int col;
+    [SerializeField]
+    private GameObject puzzlePrefab;
+    [SerializeField]
+    private float cellsize;
+    private Vector2[,] tables;
+
+    private List<Node> nodes;
+    private Dictionary<Node, GameObject> nodesDictObj;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        grid = new Puzzle[rows, columns]; 
-        blocked = new bool[rows, columns];
-        AddPrefab(spawnPuzzleList[0], 5);
-        AddPrefab(spawnPuzzleList[1], 5);
-        AddPrefab(spawnPuzzleList[2], 5);
-        AddPrefab(spawnPuzzleList[3], 5);
-        SpawnPuzzles();
+        nodesDictObj = new();
+        nodes = new();
+        nodes.Add(new Node(new Vector2Int(0, 0), NodeType.Left));
+        nodes.Add(new Node(new Vector2Int(1, 1), NodeType.Up));
+        nodes.Add(new Node(new Vector2Int(2, 2), NodeType.Left));
+        nodes.Add(new Node(new Vector2Int(3, 3), NodeType.Left));
+        nodes.Add(new Node(new Vector2Int(4, 4), NodeType.Down));
+        nodes.Add(new Node(new Vector2Int(0, 1), NodeType.Left));
+        nodes.Add(new Node(new Vector2Int(0, 2), NodeType.Left));
+        tables = new Vector2[rows, col];
+        for (int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                float PosX = (-rows / 2 + i) *cellsize;
+                float PosY = (col / 2 - j) *cellsize;
+               
+                tables[i,j] = new Vector2(PosX, PosY);
+            }    
+        }
+        foreach (var node in nodes)
+        {
+            Quaternion quaternion = Quaternion.identity;
+            switch (node.NodeType)
+            {
+                case NodeType.Left:
+                    quaternion = Quaternion.Euler(0, 0,90);
+                    break;
+                case NodeType.Right:
+                    quaternion = Quaternion.Euler(0, 0,-90);
+                    break;
+                case NodeType.Up:
+                    quaternion = Quaternion.Euler(0, 0, 0);
+                    break;
+                case NodeType.Down:
+                    quaternion = Quaternion.Euler(0, 0, 180);
+                    break;
+            } 
+            GameObject NodeObj = Instantiate(puzzlePrefab, tables[node.position.x, node.position.y], quaternion);
+            nodesDictObj.Add(node, NodeObj);
+        }    
     }
-    public void init(int rows , int columns)
+    private void Update()
+    {
+        Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Node node;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D collider2D = Physics2D.OverlapPoint(touchPos); 
+            if(collider2D != null)
+            {
+                Vector2Int temp = new Vector2Int(Mathf.RoundToInt(collider2D.transform.position.x/cellsize + rows/2), Mathf.RoundToInt(-collider2D.transform.position.y/cellsize + col/2));
+                node = nodes.Find(n => n.position == new Vector2 (temp.x,temp.y));
+                Vector2Int movePos = NodeMovePosition(node);
+                Vector2 destinition;
+                if(movePos == node.position)
+                {
+                    destinition = tables[movePos.x, movePos.y];
+                }
+                else if (movePos.x <0 || movePos.x >100 || movePos.y <0 && movePos.y >100)
+                {
+                    destinition = new Vector2(999f, 999f);
+                    nodes.Remove(node);
+                }
+                else
+                {
+                    destinition = tables[movePos.x, movePos.y];
+                    nodes[nodes.IndexOf(node)].position = movePos;
+                }
+                MoveNode(nodesDictObj[node], destinition);
+
+            }
+        }    
+    }
+    private void MoveNode(GameObject node , Vector2 destinition)
+    {
+        node.transform.position = destinition;
+    }
+    public void init(int rows , int col , float cellsize)
     {
         this.rows = rows;
-        this.columns = columns;
+        this.col = col;
+        this.cellsize = cellsize;
     }
-    public void AddPrefab(Puzzle prefab, int instanceCount)
+    public Vector2Int NodeMovePosition(Node node)
     {
-        for (int i = 0; i < instanceCount-1; i++)
+        Vector2Int direction = new();
+        switch (node.NodeType)
         {
-            Puzzle instance = Instantiate(prefab);
-            instance.gameObject.SetActive(false);
-            puzzlePrefabs.Add(instance);
-        }
-    }
-  /*  public Puzzle GetRandomPrefab()
-    {
-        Puzzle randomPrefab = puzzlePrefabs[Random.Range(0, puzzlePrefabs.Count)];
-        return randomPrefab;
-    }*/
-    public Puzzle GetInstanceFromPool()
-    { 
-        foreach (Puzzle instance in puzzlePrefabs)
+            case NodeType.None:
+                return Vector2Int.one * -1;
+                break;
+            case NodeType.Left:
+                direction = Vector2Int.left;
+                break;
+            case NodeType.Right:
+                direction = Vector2Int.right;
+                break;
+            case NodeType.Up:
+                direction = Vector2Int.down;
+                break;
+            case NodeType.Down:
+                direction = Vector2Int.up;
+                break;
+        }    
+        Vector2Int curPos = node.position;
+        while (curPos.x < rows && curPos.x >= 0  && curPos.y < col && curPos.y >= 0)
         {
-
-            if (!instance.gameObject.activeInHierarchy)
-            {
-                instance.gameObject.SetActive(true);
-
-                return instance;
-            }
-        }
-        return null; 
-    }
-    public Vector3 GetCellPosition(int row, int column)
-    {
-        float cellSize = 1f;
-        Vector3 origin = new Vector3(-columns / 3f, -rows / 3f, 0f);
-        Vector3 cellPosition = origin + new Vector3(column, row, 0f) * cellSize;
-        return cellPosition;
-    }
-    public void SpawnPuzzles()
-    {    
-        for (int row = 0; row < rows; row++)
-        {
-            for (int column = 0; column < columns; column++)
-            {
-                Puzzle puzzle = GetInstanceFromPool();
-                if (puzzle != null)
-                {           
-                    grid[row, column] = puzzle;
-                    puzzle.SetRowColumn(row, column);
-                    puzzle.transform.position = GetCellPosition(row, column);                 
-                    puzzle.gameObject.SetActive(true);
-                }
-            }
-        }
-    }
-    public void DeletePos(int row, int column)
-    {
-        grid[row, column] = null;
-    }
-    public void SetPuzzlePosition(Puzzle puzzle, int newRow, int newColumn)
-    {
-        grid[puzzle.GetRow(), puzzle.GetColumn()] = null;
-        puzzle.SetRowColumn(newRow, newColumn);
-        grid[newRow, newColumn] = puzzle;
-    }
-    public int IsPositionEmpty(int row, int column)
-    {
-        int numRows = grid.GetLength(0);
-        int numColumns = grid.GetLength(1);
-        Debug.Log(row + "," + column);
-        Debug.Log(numRows+","+numColumns);
-
-        if (row < 0 || row >= numRows || column < 0 || column >= numColumns)
-        {
-            Debug.Log("Bay ra khỏi map");
-            return 0;
-        }
-
-        if (grid[row, column] == null)
-        {
-            Debug.Log("Tiến tới");
-            return 1;
-        }
-        else
-        {
-            Debug.Log("Bị kẹt");
-            return 2;
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {       
-    }
+            curPos += direction;
+            Node temp = nodes.Find(node=>node.position == curPos);
+            if (temp != null) return curPos-=direction;
+            Debug.Log(curPos);
+        }    
+        return Vector2Int.one*999;
+     
+    }    
 }
